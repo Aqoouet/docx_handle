46be420981811444aea2f3b77ac023e58d66316b
Aqouet <maximryazanoff@yandex.ru>
2026-05-10 17:29:41 +0300

    chore: remove bootstrap_venv, docling scripts, and postprocess - out of scope for this service

--- scripts/run_full_test.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set ROOT_DIR=%~dp0..
-if not exist "%ROOT_DIR%\logs" mkdir "%ROOT_DIR%\logs"
-cd /d "%ROOT_DIR%"
-call "%ROOT_DIR%\.venv\Scripts\python.exe" -u "%ROOT_DIR%\scripts\run_full_test.py" --skip-docling %* >> "%ROOT_DIR%\logs\full_test.log" 2>&1
